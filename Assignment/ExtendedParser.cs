aba5c7ad47f75de0e273d47758ba8d33e0e61bed
Matt <beszac.525@gmail.com>
2020-10-24 02:29:05 +0700

    Added If statements

--- Assignment/ExtendedParser.cs
+++ Assignment/ExtendedParser.cs
@@ -16,6 +16,8 @@ namespace Assignment
         Panel GraphicsPanel;
         Dictionary<string, int> vars = new Dictionary<string, int>();
         Regex VariableName = new Regex("\\w*\\d*");
+        bool IfStatementValid = false;
+        bool EndifFound = false;
 
         public ExtendedParser(Panel InputPanel, Bitmap InputBitmap) : base(InputPanel, InputBitmap)
         {
@@ -27,11 +29,95 @@ namespace Assignment
         {
             foreach (string l in Inputbox.Lines)
             {
-                string[] split = l.Split(' ');
-                if (VariableName.IsMatch(split[0])) //If input is any type of text and it has some type of operator a variable will be added or changed
+                string[] split = { " " };
+                if (l.Trim() != "Endif")
+                {
+                    split = l.Split(' ');
+                }
+                else if(l.Trim() == "Endif")
+                {
+                    split[0] = "Endif";
+                }
+       
+                if (split[0] == "If")
+                {
+                    int ValueToCompare = int.Parse(split[3]);
+                    if (vars.ContainsKey(split[1]))
+                    {
+                        if (split[2] == "=")
+                        {
+                            if (vars[split[1]] == ValueToCompare)
+                            {
+                                EndifFound = false;
+                                IfStatementValid = true;
+                            }
+                        }
+                        else if (split[2] == ">")
+                        {
+                            if (vars[split[1]] > ValueToCompare)
+                            {
+                                EndifFound = false;
+                                IfStatementValid = true;
+                            }
+                        }
+                        else if (split[2] == ">=")
+                        {
+                            if (vars[split[1]] >= ValueToCompare)
+                            {
+                                EndifFound = false;
+                                IfStatementValid = true;
+                            }
+                        }
+                        else if (split[2] == "<")
+                        {
+                            if (vars[split[1]] < ValueToCompare)
+                            {
+                                EndifFound = false;
+                                IfStatementValid = true;
+                            }
+                        }
+                        else if (split[2] == "<=")
+                        {
+                            if (vars[split[1]] <= ValueToCompare)
+                            {
+                                EndifFound = false;
+                                IfStatementValid = true;
+                            }
+                        }
+                        else if (split[2] == "!=")
+                        {
+                            if (vars[split[1]] != ValueToCompare)
+                            {
+                                EndifFound = false;
+                                IfStatementValid = true;
+                            }
+                        }
+                    }
+                }
+                else if (split[0] == "Endif")
+                {
+                    IfStatementValid = false;
+                    EndifFound = true;
+                }
+                else if (split[0] == "Loop")
+                {
+
+                }
+                else if (split[0] == "Method")
+                {
+
+                }
+                else if (split[0] == "drawTo" || split[0] == "moveTo" || split[0] == "Rectangle" || split[0] == "Triangle"|| split[0] == "Circle" || split[0] == "Fill" || split[0] == "Colour")
+                {
+                    if ((IfStatementValid == true && EndifFound == false) || (EndifFound == true && IfStatementValid == false))
+                    {
+                        ParseCommand(l.Trim());
+                    }
+                }
+                else if (VariableName.IsMatch(split[0])) //If input is any type of text and it has some type of operator a variable will be added or changed
                 {
                     string Name;
-                    int Value;
+                    int Value = 0;
                     if (split[1] == "=")
                     {
                         Name = split[0];
@@ -45,25 +131,39 @@ namespace Assignment
                             MessageBox.Show("Invalid variable value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
-                    else if (split[0] == "If")
+                    else if (split[1] == "+")
                     {
-
+                        if (vars.ContainsKey(split[0]))
+                        {
+                            vars[split[0]] += int.Parse(split[2]);
+                        }
                     }
-                    else if (split[0] == "Loop")
+                    else if (split[1] == "-")
                     {
-
+                        if (vars.ContainsKey(split[0]))
+                        {
+                            vars[split[0]] -= int.Parse(split[2]);
+                        }
                     }
-                    else if (split[0] == "Method")
+                    else if (split[1] == "*")
                     {
-
+                        if (vars.ContainsKey(split[0]))
+                        {
+                            vars[split[0]] *= int.Parse(split[2]);
+                        }
                     }
-                    else
+                    else if (split[1] == "/")
                     {
-                        ParseCommand(l.Trim());
+                        if (vars.ContainsKey(split[0]))
+                        {
+                            vars[split[0]] /= int.Parse(split[2]);
+                        }
                     }
                 }
             }
         }
+
+
 
         public override void ParseCommand(string InputCommand)
         {
@@ -98,7 +198,7 @@ namespace Assignment
                     dp.DrawLine(x, y);
                     GraphicsPanel.Refresh();
                 }
-                else if(vars.ContainsKey(SplitParameters[1]))
+                else if (vars.ContainsKey(SplitParameters[1]))
                 {
                     x = SetX(SplitParameters[0]);
                     y = SetY(vars[SplitParameters[1]].ToString());
@@ -188,7 +288,7 @@ namespace Assignment
                     x = SetX(SplitParameters[0]);
                     dp.DrawCircle(x);
                     GraphicsPanel.Refresh();
-                }          
+                }
             }
             else if (Command.Trim() == "Fill")
             {
